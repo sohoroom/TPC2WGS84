4387268e533ea42bb21843d2cd00017ea0002474
yunzhe.shi <sohoroom@gmail.com>
2026-04-10 08:36:58 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+﻿const CACHE_NAME = 'tpc-conv-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.15.0/proj4.js'
+];
+
+// 安裝並快取資源
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 離線優先策略
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
